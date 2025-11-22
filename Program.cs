7168146264eb29be0eacb35f2f0b207138467f1e
Anastasia <filippov31012007@gmail.com>
2025-11-22 15:49:50 +0500

    создан метод LoadLeaders.permilka

--- Program.cs
+++ Program.cs
@@ -119,6 +119,21 @@ namespace Snake
             SW.WriteLine(json);
             SW.Close();
         }
+        public static void LoadLeaders()
+        {
+            if (File.Exists(". /leaders.txt"))
+            {
+                StreamReader SR = new StreamReader(". /leaders.txt");
+                string json = SR.ReadLine();
+                SR.Close();
+                if (!string.IsNullOrEmpty(json))
+                    Leaders = JsonConvert.DeserializeObject<List<Leaders>>(json);
+                else
+                    Leaders = new List<Leaders>();
+            }
+            else
+                Leaders = new List<Leaders>();
+        }
     }
 }
 
